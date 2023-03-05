38a2a5d69345591836f79d628f74c50760d07f48
Nacho <isanccal@myuax.com>
2023-03-05 19:00:06 +0100

    Cambio final

--- Ejercicio9.1.py
+++ Ejercicio9.1.py
@@ -1,5 +1,19 @@
 def palabras_con_letra(letra, diccionario, siguiente, anterior):
     lista = []
     i = 0
-    while i < len(diccionario) 
+    while i < len(diccionario) and diccionario[i][0] < letra:
+        i += 1
+    while i < len(diccionario) and diccionario[i][0] == letra:
+        lista.append(diccionario[i])
+        i += 1
+    if siguiente:
+        while i < len(diccionario) and diccionario[i][0] == siguiente:
+            lista.append(diccionario[i])
+            i += 1
+    if anterior:
+        i = i - 2
+        while i >= 0 and diccionario[i][0] == anterior:
+            lista.append(diccionario[i])
+            i -= 1
+    return lista
 
