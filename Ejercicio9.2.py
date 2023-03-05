b82aae987d9613f5956abace39aa7da05ce74531
Nacho <isanccal@myuax.com>
2023-03-05 19:04:23 +0100

    Cambio 2

---
+++ Ejercicio9.2.py
@@ -0,0 +1,19 @@
+class Palabra
+    def __init__(self, palabra, anterior, siguiente):
+        self.palabra = palabra
+        self.anterior = anterior
+        self.siguiente = siguiente
+
+def palabras_con_letra(letra, diccionario, anterior, siguiente):
+    lista = []
+    i = 0
+    while i < len(diccionario) and diccionario[i].palabra[0] < letra:
+        i += 1
+    while i < len(diccionario) and diccionario[i].palabra[0] == letra:
+        lista.append(diccionario[i])
+        i += 1
+    if siguiente:
+        while i < len(diccionario) and diccionario[i].palabra[0] == siguiente:
+            lista.append(diccionario[i])
+            i += 1
+        
